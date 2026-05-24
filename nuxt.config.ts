6a0e1a0a426e552bb1b5e34c3dce65611a4ca85f
William Oldham <github@binaryoverload.co.uk>
2026-05-24 12:28:27 +0100

    chore: add typecheck to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	modules: ['@nuxt/fonts', '@nuxtjs/i18n', '@nuxt/eslint'],
+	typescript: {
+		typeCheck: true
+	},
 	eslint: {
 		config: {
 			standalone: false
