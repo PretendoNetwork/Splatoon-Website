aeb9e683b32e1edf2d1bfebcdc06276c1ec3363a
Jemma Poffinbarger <git@jemsoftware.dev>
2026-06-15 20:49:13 -0500

    fix: desktop safari vs mobile

--- app/utils/matrix.ts
+++ app/utils/matrix.ts
@@ -23,7 +23,7 @@ class Matrix {
 		if (navigator.userAgent.indexOf('Chrome') != -1) {
 			return 0;
 		} else if (navigator.userAgent.indexOf('Safari') != -1) {
-			return -2;
+			return navigator.userAgent.indexOf('Macintosh') != -1 ? -2 : -4;
 		} else if (navigator.userAgent.indexOf('Firefox') != -1) {
 			return -1;
 		} else {
